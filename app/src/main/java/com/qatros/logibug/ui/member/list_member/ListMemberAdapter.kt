3b7e01b78d6a185b8068c9445ef7dca1d2c41683
Ilham Akbar Pradana <ilhamakbar.pradana@gmail.com>
2024-01-02 18:27:23 +0700

    "Fixed member list"

--- app/src/main/java/com/qatros/logibug/ui/member/list_member/ListMemberAdapter.kt
+++ app/src/main/java/com/qatros/logibug/ui/member/list_member/ListMemberAdapter.kt
@@ -1,10 +1,13 @@
 package com.qatros.logibug.ui.member.list_member
 
+import android.content.res.ColorStateList
+import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.qatros.logibug.core.data.response.member.DetailMember
 import com.qatros.logibug.databinding.ItemMemberBinding
+import java.util.Random
 
 class ListMemberAdapter(private val listMember: List<DetailMember>) :
     RecyclerView.Adapter<ListMemberAdapter.MemberListViewHolder>() {
@@ -13,9 +16,35 @@ class ListMemberAdapter(private val listMember: List<DetailMember>) :
         RecyclerView.ViewHolder(binding.root) {
 
             fun bind(member: DetailMember){
+
                 with(binding){
                     tvUsernameMember.text = member.email
                     tvRoleMember.text = member.role
+
+                    when (member.role) {
+                        "po" -> tvRoleMember.text = "Owner"
+                        "dev" -> tvRoleMember.text = "Programmer"
+                        "qa" -> tvRoleMember.text = "QA"
+                    }
+
+                    val firstLetters = member.email.split("")
+                        .filter { it.isNotBlank() }
+                        .take(1)
+                        .map { it.take(1) }
+                    tvIconUsername.text = firstLetters[0]
+
+                    val random = Random()
+                    val randomNumber = random.nextInt(4) + 1
+                    when (randomNumber) {
+                        1 -> tvIconUsername.backgroundTintList = ColorStateList.valueOf(
+                            Color.parseColor("#554AF0"))
+                        2 -> tvIconUsername.backgroundTintList = ColorStateList.valueOf(
+                            Color.parseColor("#1A8A0F"))
+                        3 -> tvIconUsername.backgroundTintList = ColorStateList.valueOf(
+                            Color.parseColor("#F99B13"))
+                        4 -> tvIconUsername.backgroundTintList = ColorStateList.valueOf(
+                            Color.parseColor("#DD00D2"))
+                    }
                 }
             }
 
